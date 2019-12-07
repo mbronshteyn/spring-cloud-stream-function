06052079002a6c0c1f79ece37c20a95dbe00e825
mbronshteyn <michael.bronshteyn@gmail.com>
2019-12-07 13:06:53 -0800

    add files via upload
    init

---
+++ src/test/java/mbronshteyn/spring/cloud/function/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package mbronshteyn.spring.cloud.function.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
